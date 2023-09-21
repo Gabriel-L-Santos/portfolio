30277a9c2ecc3757d719a7494ab8e7a154567bcc
Gabriel Leme dos Santos <gabriellemesantos@gmail.com>
2023-09-21 15:23:17 -0300

    finalizando responsividade da seção projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,43 @@
 // Initialize Swiper
 
-let swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-}); 
+let swiper = createSwiper(".mySwiper", ".swiper-pagination", ".swiper-button-next", ".swiper-button-prev");
+
+function createSwiper(container, pagination, nextButton, prevButton) {
+    return new Swiper(container, {
+        slidesPerView: handleWidth(),
+        spaceBetween: 30,
+        pagination: {
+            el: pagination,
+            clickable: true,
+        },
+        navigation: {
+            nextEl: nextButton,
+            prevEl: prevButton,
+        },
+    });
+}
+
+// Ajustar os slides do Swiper de acordo com a resolução da página
+
+function handleWidth() {
+    let getWidth = window.innerWidth || document.documentElement.clientWidth;
+    let slideShow = 3;
+
+    if (getWidth < 1001) {
+        slideShow = 2;
+    }
+
+    if (getWidth < 700) {
+        slideShow = 1;
+    }
+
+    return slideShow;
+}
+
+window.addEventListener('resize', () => {
+    swiper.params.slidesPerView = handleWidth();
+    swiper.update();
+});
 
 // Efeito fade-in scroll da página
 
